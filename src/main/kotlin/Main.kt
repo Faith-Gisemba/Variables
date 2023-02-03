fb29eed1ae465c256b69f95e691d4ae5f4354ca0
Faith-Gisemba <faithmgisemba@gmail.com>
2023-02-03 08:49:45 +0300

    variable_kt

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main(args: Array<String>) {
+    var fullName: String = "Gisemba Faith"
+    println(fullName)
+
+    var studentAge: Int = 20
+    println(studentAge)
+
+    var studentNo: Long = 798314467L
+    println(studentNo)
+
+    val kg = 60
+    val pounds = kg * 2.2046
+    println("Pounds($pounds) => kg($kg)")
+
+    var rwandan: Boolean = false
+    println(rwandan)
+
+
+
+
+
+
+}
